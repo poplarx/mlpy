b5e41bd28ca910b93059130e66b91782f695af0f
poplarx <imremu@163.com>
2015-09-06 16:53:02 +0800

    modify a variable name

--- naive_bayes.py
+++ naive_bayes.py
@@ -3,14 +3,15 @@ def load_dataset():
 
 def naive_bayes(data,t_sample):
     data_num = len(data)
+    data_num_f = float(data_num)
     symptom,job,disease = t_sample
     selected_data = [[x,y,z] for x,y,z in data if (z == disease)]
     s_data_num = len(selected_data)
     p_d = float(s_data_num) / data_num
     p_s_d = len([x for x,y,z in selected_data if (x == symptom)]) / float(s_data_num)
     p_j_d = len([y for x,y,z in selected_data if (y == job)]) / float(s_data_num)
-    p_s = len([x for x,y,z in data if (x==symptom)]) / float(data_num)
-    p_j = len([y for x,y,z in data if (y==job)]) / float(data_num)
+    p_s = len([x for x,y,z in data if (x==symptom)]) / data_num_f
+    p_j = len([y for x,y,z in data if (y==job)]) / data_num_f
     
     print p_d,p_s_d,p_j_d,p_s,p_j
     return p_s_d*p_j_d*p_d / (p_s * p_j)
